cb404419046d28bbabcb127a43107ed2a2af4ca4
Nicola Silveri <nikis900@gmail.com>
2026-01-27 14:36:01 +0100

    Add volt to wind speed scale

--- main_ina3221.py
+++ main_ina3221.py
@@ -5,7 +5,27 @@ import time
 INA3221_ADDR = 0x42
 
 # I2C Config
-i2c = I2C(1, scl=1, sda=0)
+i2c = I2C(0, scl=1, sda=0)
+
+# Wind scale factor [V, mt/s]
+VOLT = 0
+SPEED = 1
+min_scale = [ 0.12, 2.5]
+max_scale = [ 0.62, 10.3]
+
+# Initial I2C scanner
+def scan_i2c():
+    try:
+        devices = i2c.scan()
+        if devices:
+            print("I2C devices found:", ", ".join("0x{:02X}".format(d) for d in devices))
+        else:
+            print("No I2C devices found.")
+    except Exception as e:
+        print("I2C scan error:", e)
+
+# Run initial scan
+scan_i2c()
 
 # INA3221 Initialization
 ina = None
@@ -14,27 +34,46 @@ try:
 except Exception as e:
     print("INA3221 Initialization Error:", e)
 
+# Read INA3221 Data
 def read_ina3221():
     try:
-        v1 = ina.get_bus_voltage(1)
-        i1 = ina.get_current(1)
+        #v_1 = ina.get_bus_voltage(1)
+        v_bus_1 = ina.get_bus_voltage(1)
+        #i1 = ina.get_current(1)
 
-        v2 = ina.get_bus_voltage(2)
-        i2 = ina.get_current(2)
-
-        v3 = ina.get_bus_voltage(3)
-        i3 = ina.get_current(3)
-
-        print("CH1: V={:.2f}V I={:.2f}A | CH2: V={:.2f}V I={:.2f}A | CH3: V={:.2f}V I={:.2f}A".format(
-            v1, i1, v2, i2, v3, i3
-        ))
+        return v_bus_1
+    
     except Exception as e:
         print("INA3221 Read Error:", e)
+
+# Convert voltage to wind speed
+def voltageToWindSpeed(voltage, min_scale, max_scale):
+    OUT_OF_SCALE = False
+
+    if voltage < min_scale[VOLT] or voltage > max_scale[VOLT]:
+        OUT_OF_SCALE = True
+    else:
+        OUT_OF_SCALE = False
+
+    scale = (max_scale[SPEED] - min_scale[SPEED]) / (max_scale[VOLT] - min_scale[VOLT])
+    wind_speed = min_scale[SPEED] + scale * (voltage - min_scale[VOLT])
+
+    return wind_speed, OUT_OF_SCALE
+
+# Print wind speed information
+def printWindInfo(windSpeed, outOfScale):
+    if outOfScale:
+        print("Wind Speed: {:.2f} mt/s, WARNING: Wind Speed Out of Scale".format(windSpeed))
+    else:
+        print("Wind Speed: {:.2f} mt/s".format(windSpeed))
 
 try:
     while True:
         if ina is not None:
-            read_ina3221()
-        time.sleep(1)
+            voltFromAnemometer = read_ina3221()
+            windSpeed, outOfScale = voltageToWindSpeed(voltFromAnemometer, min_scale, max_scale)
+            printWindInfo(windSpeed, outOfScale)
+
+        time.sleep(0.2)
 except KeyboardInterrupt:
     print("Interrupted by user.")
